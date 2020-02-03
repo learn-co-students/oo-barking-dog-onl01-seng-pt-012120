3a4e329b6dd87486b4c3c24be2b40976dcd90ebf
fadimata tillman <fadi.tillman@gmail.com>
2020-02-03 22:16:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+
+
+class Dog
+
+
+
+
+
+  def bark
+  puts  "woof!"
+  end
+
+  def name=(dogs_name)
+    @this_dog_name = dogs_name
+  end
+
+  def name
+    @this_dog_name
+  end
+end
+
+  # def bark=(dogs_bark)
+  #   @this_dog_bark = dogs_bark
+  # end
+  #
+  # def bark
+  #   @this_dog_bark
+  # end
+
+
+
+ # Your code goes here!
