57d64c13ad543059f8ab2e8d6576530f796cb62c
Alvaro Armas <aarmas92@outlook.com>
2020-02-06 16:09:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,17 @@
 # Your code goes here!
-class Dog 
-  attr_accessor :name
-  
+class Dog
+  def name
+    @name
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
   def bark
     puts "woof!"
-  end   
-end 
+  end
+end
 
 fido=Dog.new
+fido.name="Fido"
